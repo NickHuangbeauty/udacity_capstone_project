1ec5a863dc50bf02b8fec46cda4eda1384ce29fd
NickHuangbeauty <a92224733@gmail.com>
2022-05-25 11:32:45 +0800

    add some commit

--- airflow/dags/dag_etl_aws_emr.py
+++ airflow/dags/dag_etl_aws_emr.py
@@ -5,7 +5,6 @@ import datetime
 
 from airflow.models import DAG
 from airflow.models import Variable
-from airflow.utils.task_group import TaskGroup
 from airflow.operators.dummy_operator import DummyOperator
 
 from airflow.providers.amazon.aws.operators.emr_add_steps import EmrAddStepsOperator
@@ -44,13 +43,35 @@ with DAG(DAG_ID,
          tags=['Step2_aws_emr_to_s3']
          ) as dag:
 
-    start = DummyOperator(task_id='start to Add  EMR Step')
+    start = DummyOperator(task_id='Start to Add EMR Step')
+
+    # TODO: Check create AWS EMR all connection and config for using AWS boto3 API to create EMR instances.
 
     # Creates an EMR JobFlow, reading the config from the EMR connection.A dictionary of JobFlow overrides can be passed that override the config from the connection.
-    create_job_flow = EmrCreateJobFlowOperator()
+    create_job_flow = EmrCreateJobFlowOperator(
+        aws_conn_id='aws_default',
+        emr_conn_id='aws_emr_default',
+        job_flow_overrides=JOB_FLOW_OVERRIDES,
+        region_name='us-east-2'
+    )
 
     # An operator that adds steps to an existing EMR job_flow.
-    add_steps = EmrAddStepsOperator()
+    add_steps = EmrAddStepsOperator(
+        aws_conn_id='aws_default',
+        job_flow_overrides=JOB_FLOW_OVERRIDES,
+        job_flow_name='{{ task_instance.xcom_pull(task_ids="Create EMR Job Flow") }}',
+        cluster_states=['WAITING'],
+        steps=SPARK_STEP,
+    )
 
     # Asks for the state of the step until it reaches any of the target states. If it fails the sensor errors, failing the task.
-    wait_for_step = EmrStepSensor()
+    wait_for_step = EmrStepSensor(
+        job_flow_id='{{ task_instance.xcom_pull(task_ids="Create EMR Job Flow") }}',
+        step_id='{{ task_instance.xcom_pull(task_ids="Add EMR Step") }}',
+        target_states=['COMPLETED'],
+        failed_states=['FAILED'],
+    )
+
+    end = DummyOperator(task_id='End to Add EMR Step')
+
+    start >> create_job_flow >> add_steps >> wait_for_step >> end
